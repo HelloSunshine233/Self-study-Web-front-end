bea0dc744913266227c02c034d829adea01e0cd2
HelloSunshine233 <1589089447@qq.com>
2021-02-12 13:20:17 +0800

    master

--- 1--4个小时带你快速入门vue/demo-悦听player/js/main.js
+++ 1--4个小时带你快速入门vue/demo-悦听player/js/main.js
@@ -26,99 +26,48 @@
     请求参数:id(mvid,为0表示没有mv)
     响应内容:mv的地址
 */
+/*
+  一 歌曲搜索的知识点
+    1. 按下回车（v-on:enter）
+    2. 查询数据（axios 接口 v-model）
+    3. 渲染数据（v-for 数组 that）
+
+  二 歌曲播放
+    1. 点击播放（v-on 自定义参数）
+    2. 歌曲的播放地址（接口 歌曲id）
+    3. 歌曲的地址设置(v-bind)
+*/
 var app = new Vue({
-  el: "#player",
-  data: {
+  // el:"player",
+  el:"#player",
+  data:{
     // 查询关键字
     query: "",
-    // 歌曲数组
-    musicList: [],
-    // 歌曲地址
-    musicUrl: "",
-    // 歌曲封面
-    musicCover: "",
-    // 歌曲评论
-    hotComments: [],
-    // 动画播放状态
-    isPlaying: false,
-    // 遮罩层的显示状态
-    isShow: false,
-    // mv地址
-    mvUrl: ""
+    //歌曲数组
+    musicList: []
+
+
   },
-  methods: {
-    // 歌曲搜索
-    searchMusic: function() {
+  methods:{
+    // 搜索音乐方法
+    searchMusic:function() {
       var that = this;
+      // 为什么是get呢，以为上面的第一个的接口文档是get
+      // keywords 关键词的问题不是太了解
       axios.get("https://autumnfish.cn/search?keywords=" + this.query).then(
-        function(response) {
-          // console.log(response);
-          that.musicList = response.data.result.songs;
-          console.log(response.data.result.songs);
-        },
-        function(err) {}
+        function(response){
+        console.log(response.data.result.songs);
+        that.musicList = response.data.result.songs;
+        // console.log(response.data.result.songs);
+      },function(err){}
       );
     },
     // 歌曲播放
-    playMusic: function(musicId) {
-      //   console.log(musicId);
-      var that = this;
-      // 获取歌曲地址
-      axios.get("https://autumnfish.cn/song/url?id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.data[0].url);
-          that.musicUrl = response.data.data[0].url;
-        },
-        function(err) {}
-      );
-
-      // 歌曲详情获取
-      axios.get("https://autumnfish.cn/song/detail?ids=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.songs[0].al.picUrl);
-          that.musicCover = response.data.songs[0].al.picUrl;
-        },
-        function(err) {}
-      );
-
-      // 歌曲评论获取
-      axios.get("https://autumnfish.cn/comment/hot?type=0&id=" + musicId).then(
-        function(response) {
-          // console.log(response);
-          // console.log(response.data.hotComments);
-          that.hotComments = response.data.hotComments;
-        },
-        function(err) {}
-      );
-    },
-    // 歌曲播放
-    play: function() {
-      // console.log("play");
-      this.isPlaying = true;
-    },
-    // 歌曲暂停
-    pause: function() {
-      // console.log("pause");
-      this.isPlaying = false;
-    },
-    // 播放mv
-    playMV: function(mvid) {
-      var that = this;
-      axios.get("https://autumnfish.cn/mv/url?id=" + mvid).then(
-        function(response) {
-          // console.log(response);
-          console.log(response.data.data.url);
-          that.isShow = true;
-          that.mvUrl = response.data.data.url;
-        },
-        function(err) {}
-      );
-    },
-    // 隐藏
-    hide: function() {
-      this.isShow = false;
+    // 当下面的方法的定义名出现波浪线的时候就是上面方法最后没有添加逗号 ,
+    // 这个function中的（参数）  就是传参的作用
+    playMusic:function(musicId) {
+      console.log(musicId);
     }
+
   }
 });
